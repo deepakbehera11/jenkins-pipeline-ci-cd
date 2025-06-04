914bc0a6333f1d44bba8732ea32d7e8d67cdca55
Deepak <108412773+deepakbehera11@users.noreply.github.com>
2025-06-04 18:43:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello I am deepak, I am a devops engineer, from Node.js Demo App!');
+  res.send('Hello I am deepak, I am a devops engineer, This is jenkin pipeline from Node.js Demo App!');
 });
 
 app.listen(port, () => {
